a4b4dd4ab8dc411420be5c28d9e9b17c1ccb1c90
Yakov <git@yakov.cloud>
2025-08-06 08:43:14 +0000

    Add meta tags

--- site/vite.config.js
+++ site/vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'xbox-logo.jpg',
+        dest: 'assets'
+      }
+    ]
+  })],
   build: {
     outDir: './public',
     emptyOutDir: false,
